460f2a57c321f947fad161261d07757b4073b084
Polar <readingworld2@gmail.com>
2024-01-31 14:30:31 -0500

    Pass the string input[type=text] to the querySelectorAll() method.You can then access the length property of the NodeList to get the number of entries.

--- script.js
+++ script.js
@@ -21,5 +21,6 @@ const addEntry = () => {
   const targetInputContainer = document.querySelector(
     `#${entryDropdown.value} .input-container`
   );
-  const entryNumber = targetInputContainer.querySelectorAll();
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
 };
