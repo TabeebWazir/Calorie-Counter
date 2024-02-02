dd19d0ac69c9b39599192842b6c193240700a3cf
Polar <readingworld2@gmail.com>
2024-02-02 11:43:01 -0500

    After your loop completes, you need to clear the budgetNumberInput. Set the value property of budgetNumberInput to an empty string.

--- script.js
+++ script.js
@@ -109,6 +109,8 @@ const clearForm = () => {
   for (let i = 0; i < inputContainers.length; i++) {
     inputContainers[i].innerHTML = "";
   }
+
+  budgetNumberInput.value = "";
 };
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
